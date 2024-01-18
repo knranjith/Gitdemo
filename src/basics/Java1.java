357819ad3f207d2150f18de07458cfcf66e0e2ee
ranjith <ranjithkn36@gmail.com>
2024-01-18 15:03:06 +0530

    commit with update in java1 file

--- src/basics/Java1.java
+++ src/basics/Java1.java
@@ -5,6 +5,10 @@ public class Java1 {
 	public static void main (String[]args)
 	{
 		System.out.println("Hello");
+		
+		System.out.println("hello hello helo");
+		System.out.println("hello hello helo1");
+		System.out.println("hello hello helo2");
 	}
 	
 }
